e1c446661786145a99cee0b3171b9f9b22703efc
SabbathCode <daniel.arturjsb@gmail.com>
2021-08-21 17:20:39 -0300

    enviando css e javascript

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
 //functions created
 
+var bttn = document.querySelector('.bttnabout');
+var aboutm = document.querySelector('.aboutm');
+var selctwhat = document.getElementById('what');
+var selctmssn = document.getElementById('mission');
+var selctwho = document.getElementById('who');
+bttn.addEventListener('click', function(){
+   
+    
+});
+
+
+
+if(id)
+
+
+
 function tospace(){
     var name = document.querySelectorAll('.name');
     let input = document.createElement("input");
@@ -8,3 +24,36 @@ function tospace(){
 
 
 var name = querySelectorall('.')
+
+
+function showwhat(){
+    if(aboutm.style.display === 'block'){
+        what.style.display = 'none';
+    } else {
+        what.style.display = 'block';
+        mission.style.display = 'none';
+        who.style.display = 'none'
+    }
+}
+
+
+function showmission(){
+    if(mission.style.display === 'block'){
+        mission.style.display = 'none';
+    } else {
+        what.style.display = 'none';
+        mission.style.display = 'block';
+        who.style.display = 'none'
+    }
+}
+
+
+function showwho(){
+    if(who.style.display === 'block'){
+        who.style.display = 'none';
+    } else {
+        what.style.display = 'none';
+        mission.style.display = 'none';
+        who.style.display = 'block';
+    }
+}
